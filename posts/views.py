2374fef5cc3373700289479d0230733446b11919
Divyansh Prakash <divyansh.prakash@yahoo.com>
2014-09-03 00:40:53 +0530

    Finished tut03, got basic index and post pages working.

--- posts/views.py
+++ posts/views.py
@@ -1,9 +1,13 @@
-from django.shortcuts import render
+from django.http import HttpResponse, Http404
+from django.shortcuts import render, get_object_or_404
 
-from django.http import HttpResponse
+from posts.models import Post
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the post index.")
+    latest_post_list = Post.objects.all().order_by('-pub_date')[:5]
+    context = {'latest_post_list': latest_post_list}
+    return render(request, 'posts/index.html', context)
 
 def apost(request, post_id):
-    return HttpResponse("You're looking at post %s." % post_id)
+    post = get_object_or_404(Post, pk=post_id)
+    return render(request, 'posts/apost.html', {'post': post})
--- thetalkingwall/urls.py
+++ thetalkingwall/urls.py
@@ -4,6 +4,6 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^posts/', include('posts.urls')),
+    url(r'^posts/', include('posts.urls', namespace="posts")),
     url(r'^admin/', include(admin.site.urls))
 )
