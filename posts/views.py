a470e43cf0d557b9e1c306c82128e97575ee6919
Divyansh Prakash <divyansh.prakash@yahoo.com>
2014-09-02 18:36:01 +0530

    Created new views in posts

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the post index.")
+
+def apost(request, post_id):
+    return HttpResponse("You're looking at post %s." % post_id)
